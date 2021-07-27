521df99ea6036b78c5162e2bfa9a93a9107e32e4
mohammadkabbara <mohammadkabbara40@gmail.com>
2021-07-27 23:38:25 +0300

    react app 3

--- src/components/HornedBeast.js
+++ src/components/HornedBeast.js
@@ -9,39 +9,63 @@ import Col from 'react-bootstrap/Col';
 
 class HornedBeast extends React.Component {
 
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-        numOfPets : 0
+      numOfPets: 0
     }
-}
+  }
 
-incrementNumOfPets = () => {
+  incrementNumOfPets = () => {
     this.setState({
-        numOfPets: this.state.numOfPets + 1
-    })
-}
+      numOfPets: this.state.numOfPets + 1
+    });
+    this.props.displaying(this.props.title, this.props.image, this.props.description)
+    this.props.showing()
+  }
+
+   
+
 
   render() {
     return (
       <>
-       
-      <Col>
-      <div className="row">
-      
-        <Card style={{ width: '18rem',height: "30rem" }}>
-          <Card.Img className="image"  onClick={this.incrementNumOfPets} variant="top" src={this.props.image} style={{ width: '18rem' }}></Card.Img> 
-          <Card.Body>
 
-            <Card.Title>{this.props.description} </Card.Title>
-            <Card.Text>
-              Number of Pets 😸 : {this.state.numOfPets}
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-        </Card> 
-        </div>
+        <Col>
+          <div className="row">
+
+            <Card 
+            style={{ width: '18rem' }}
+            //  style={{ height: '18rem'  }}
+              style={{ background: '#928585'  }}
+             
+               >
+               
+
+              <Card.Img
+               className="image" 
+               onClick={this.incrementNumOfPets} 
+               variant="top" 
+               src={this.props.image} 
+               style={{ width: '18rem' }}></Card.Img>
+
+              <Card.Body>
+
+                <Card.Title>
+                  {this.props.description} 
+                  </Card.Title>
+                <Card.Text>
+                  Number of Pets 😸 : {this.state.numOfPets}
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </div>
         </Col>
+
+
+
+     
       </>
 
     )
@@ -51,4 +75,3 @@ incrementNumOfPets = () => {
 
 export default HornedBeast;
 
- 
--- src/components/Main.js
+++ src/components/Main.js
@@ -12,15 +12,19 @@ class Main extends React.Component {
   render() {
     return (
       <div className="first">
-         <Row xs={1} md={3} className="g-4">
+         <Row xs={1} md={2} className="g-4">
         {
           data.map((item) => {
 
             return (
 
-              <HornedBeasts image={item.image_url}
+              <HornedBeasts 
+                image={item.image_url}
                 title={item.title}
-                description={item.description}>
+                description={item.description}
+                showing={this.props.handleShow} 
+                displaying = {this.props.displayImage}>
+               
               </HornedBeasts>
 
 
